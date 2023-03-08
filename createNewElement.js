8a247f16de77fd1d026b417ab036c561daf307be
yrgenius <yrgenius80@gmail.com>
2023-03-08 20:41:11 +0900

    feat: beta version 2.0.0

--- createNewElement.js
+++ createNewElement.js
@@ -1,16 +1,16 @@
 import { state } from "./state.js";
+import { changeState } from "./activeObserver.js";
 
-export function createNewElement(parentElement, text, id) {
-
-    // console.log(parentElement);
-    // console.log(id);
-
+export function createNewElement(parentElement, text, id, status) {
     let item = document.createElement('div');
     let itemInput = document.createElement('input');
     let itemText = document.createElement('p');
     let itemButton = document.createElement('button');
 
     item.classList.add('item');
+    if (status === 'active') {
+        item.classList.add('activeEl');
+    }
     item.id = id;
     itemInput.classList.add('list__input');
     itemInput.setAttribute('type', 'radio');
@@ -18,6 +18,7 @@ export function createNewElement(parentElement, text, id) {
     itemText.textContent = text;
     itemButton.classList.add('button');
 
+    itemInput.addEventListener('click', changeState);
     itemButton.addEventListener('click', state.deleteTask);
 
     item.append(itemInput, itemText, itemButton);
